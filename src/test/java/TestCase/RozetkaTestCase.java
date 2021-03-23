d20f208b4a7b625b4603f9a13123ec26f4f49f1b
Lepester <slavik55350@gmail.com>
2021-03-23 12:17:55 +0200

    RozetkaCase#1

---
+++ src/test/java/TestCase/RozetkaTestCase.java
@@ -0,0 +1,101 @@
+package TestCase;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+
+import org.testng.annotations.Test;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class RozetkaTestCase {
+
+
+    @Test
+    public void secondTCase() {
+
+        System.setProperty("webdriver.chrome.driver", "browserDrivers/chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://rozetka.com.ua/");
+        driver.manage().window().maximize();
+
+
+        WebElement searchElement = driver.findElement(By.xpath("//button[@id='fat-menu']"));
+        searchElement.click();
+
+
+        Actions actions = new Actions(driver);
+        WebElement target = driver.findElement(By.xpath("//a[@href='https://rozetka.com.ua/alkoholnie-napitki-i-produkty/c4626923/']"));
+        actions.moveToElement(target).perform();
+
+
+        WebElement goToBeer = driver.findElement(By.xpath("//a[@href='https://rozetka.com.ua/pivo/c4626589/']"));
+        goToBeer.click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        WebElement yesAccept = driver.findElement(By.xpath("//a[@class='btn-link-i exponea-close']"));
+        yesAccept.click();
+
+
+        WebElement redirectToBeer = driver.findElement(By.xpath("//a[@title='Упаковка пива Corona Extra светлое пастеризованное 4.5% 0.33 л х 6 шт (7501064199851)']"));
+        redirectToBeer.click();
+
+
+        WebElement acceptBuy = driver.findElement(By.xpath("//span[@class='buy-button__label']"));
+        acceptBuy.click();
+
+
+        for (int i = 0; i < 2; i++) {
+            WebElement plusButton = driver.findElement(By.xpath("//button[@aria-label='Добавить ещё один товар']"));
+            plusButton.click();
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        }
+
+
+
+        WebElement orderYes = driver.findElement(By.xpath("//a[@href='https://rozetka.com.ua//checkout/']"));
+        orderYes.click();
+
+
+
+
+        WebElement enterSurname = driver.findElement(By.xpath("//input[@formcontrolname='surname']"));
+        enterSurname.sendKeys("Гордон");
+
+        WebElement enterName = driver.findElement(By.xpath("//input[@formcontrolname='name']"));
+        enterName.sendKeys("Дмитро");
+
+        WebElement enterPhone = driver.findElement(By.xpath("//input[@formcontrolname='phone']"));
+        enterPhone.sendKeys("0505218042");
+
+        WebElement deliveryAdress = driver.findElement(By.xpath("//button[@aria-label='Выбрать адрес доставки']"));
+        deliveryAdress.click();
+
+        WebElement chooseAdress = driver.findElement(By.xpath("//input[@id='searchPickupDelivery']"));
+        chooseAdress.sendKeys("№5, ул. Ярослава Мудрого, 30/32");
+
+        WebElement clickAdress = driver.findElement(By.xpath("//div[@class='autocomplete__item']"));
+        clickAdress.click();
+
+
+        WebElement enterPatronymic = driver.findElement(By.xpath("//input[@id='recipientPatronymic']"));
+        enterPatronymic.sendKeys("Бабабоевич");                                                                   //CAN'T RESOLVE
+
+        //driver.quit();
+    }
+}
